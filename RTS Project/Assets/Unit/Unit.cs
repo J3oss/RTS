63f448064f542f7b9d27b64d0d82ad70e4436e1d
Youssef Assem <yo_90_32@hotmail.com>
2019-08-10 16:44:53 +0200

    attacking

--- RTS Project/Assets/Unit/Unit.cs
+++ RTS Project/Assets/Unit/Unit.cs
@@ -3,48 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Unit : MonoBehaviour, ISelectable
+public class Unit : MonoBehaviour
 {
-    [SerializeField] public GameObject targetMove;
-    [SerializeField] float maxHealthPoints = 100f;
-
+    [SerializeField]GameObject targetAttack;
     NavMeshAgent agent;
     //Animator animator;
 
+    ////////////////////Attack////////////////////
+    float attackConter = 0.0f;
+    float sqrAttackRange = 0.0f;
+    [SerializeField] int attackRange = 5;
+    [SerializeField] int DetectionRange = 20;
+    [SerializeField] int attackDamage = 25;
+    [SerializeField] float attackSpeed = 3.0f;
+    [SerializeField] LayerMask enemiesLayerMask = 1 << 8;
+    //////////////////////////////////////////////
+
+    ////////////////////HEALTH////////////////////
+    [SerializeField] float maxHealthPoints = 100f;
     float currentHealthPoints = 100f;
     public float healthAsPercentage
     { get { return currentHealthPoints / maxHealthPoints; } }
+    //////////////////////////////////////////////
 
+    void Start()
+    {
+        targetAttack = null;
+        agent = GetComponent<NavMeshAgent>();
+        //animator = this.transform.Find("toonRTS").GetComponent<Animator>();
+        attackConter = attackSpeed;
+        sqrAttackRange = attackRange * attackRange;
+
+        if (gameObject.layer == 8)
+        {
+            return;
+        }
+
+        PlayerUnits.playerUnits.Add(gameObject);        
+    }
+
+    public void Select()
+    {
+        PlayerUnits.selectedUnits.Add(gameObject);
+        GetComponentInChildren<Canvas>().enabled = true;
+    }
     public void Deselect()
     {
         GetComponentInChildren<Canvas>().enabled = false;
     }
 
-    public void Select()
-    {
-        
-        PlayerUnits.selectedUnits.Add(gameObject);
-        GetComponentInChildren<Canvas>().enabled = true;
-    }
-
     public void Move(Vector3 point)
     {
-        targetMove.transform.position = point;
+        targetAttack = null;
+        agent.stoppingDistance = 0;
 
-        //use this if using A*
-        //astarPath.updatePosition();
-
-        //use this if using unity navigation
-        agent.destination = targetMove.transform.position;
-        //Debug.Log("Target Move position is at " + point);
-
+        agent.destination = point;
+    }
+    public void detect()
+    {
+        Collider[] Enemies = Physics.OverlapSphere(gameObject.transform.position, DetectionRange, enemiesLayerMask);
+        Attack(Enemies[0].gameObject);
+    }
+    public void Attack(GameObject targetToAttack)
+    {
+        targetAttack = targetToAttack;
+        agent.stoppingDistance = attackRange;
+    }
+    public void takeDamage(int damage)
+    {
+        currentHealthPoints -= damage;
+        if (currentHealthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    void Start()
+    private void Update()
     {
-        PlayerUnits.playerUnits.Add(gameObject);
-        agent = GetComponent<NavMeshAgent>();
-        print("int");
-        //animator = this.transform.Find("toonRTS").GetComponent<Animator>();
+        if (gameObject.layer == 8)
+        {
+            return;
+        }
+        if (targetAttack == null)
+        {
+            detect();
+            return;
+        }
+
+        if ((gameObject.transform.position - targetAttack.transform.position).sqrMagnitude >= sqrAttackRange)
+        {
+            agent.destination = targetAttack.transform.position;
+        }
+        else
+        {
+            if (attackConter <= 0.0f)
+            {
+                Unit n = targetAttack.GetComponent<Unit>();
+                print("do damage");
+                print(attackConter);
+                n.takeDamage(attackDamage);
+                attackConter = attackSpeed;
+            }
+            else
+            {
+                print(attackConter);
+                attackConter -= Time.deltaTime;
+            }
+        }
     }
 }
